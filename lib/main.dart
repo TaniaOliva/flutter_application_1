06d4dae684d77f73e68c726ba7da366bbcb6ed74
TaniaOliva <taniaoliva1214@gmail.com>
2024-10-25 22:03:39 -0600

    Se agrego una nueva pagina

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,35 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+// ...
+
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favorites.isEmpty) {
+      return Center(
+        child: Text('No favorites yet.'),
+      );
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have '
+              '${appState.favorites.length} favorites:'),
+        ),
+        for (var pair in appState.favorites)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(pair.asLowerCase),
+          ),
+      ],
+    );
+  }
+}
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
@@ -65,7 +94,7 @@ class _MyHomePageState extends State<MyHomePage> {
         page = GeneratorPage();
         break;
       case 1:
-        page = Placeholder();
+        page = FavoritesPage();
         break;
       default:
         throw UnimplementedError('no widget for $selectedIndex');
